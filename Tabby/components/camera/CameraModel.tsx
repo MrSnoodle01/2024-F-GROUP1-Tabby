19872aa3c977a7f4006e9e5b718bc921e32edac9
Josh <richardsonjosh03@gmail.com>
2024-11-26 09:56:01 -0800

    added circular loading indicator when app is scanning cover. Also added a json to Book function to convert a api's output into a Book object.

--- Tabby/components/camera/CameraModel.tsx
+++ Tabby/components/camera/CameraModel.tsx
@@ -1,12 +1,64 @@
-import { View, Text, Pressable, Modal, TouchableWithoutFeedback, Alert } from "react-native";
+import { View, Text, Pressable, Modal, TouchableWithoutFeedback, ActivityIndicator } from "react-native";
 import * as ImagePicker from "expo-image-picker";
-import { useState, useEffect } from "react";
-import axios from 'axios';
-// import { useCameraPermissions } from "expo-camera";
+import { useState } from "react";
+import { Book } from '@/types/book';
 
 interface CameraModalProps {
     closeModal: () => void;
 }
+
+type apiReturn = {
+    authors: string         // separated by commas
+    excerpt: string
+    isbn: string            // guaranteed to be provided, ISBN 13
+    page_count: number      // -1 if not given
+    published_date: string  // YYYY-MM-DD
+    publisher: string
+    rating: number          // -1.0 if not given
+    summary: string
+    thumbnail: string
+    title: string
+};
+
+const tempBook1: Book = {
+    id: "192083745131",
+    title: "holy hell1",
+    author: "joe mama1",
+    excerpt: "this is a sick excerpt1",
+    summary: "wow sick summary1",
+    image: "hmmm ill work on this1",
+    isFavorite: false,
+};
+
+const tempBook2: Book = {
+    id: "192083745132",
+    title: "holy hell2",
+    author: "joe mama2",
+    excerpt: "this is a sick excerpt2",
+    summary: "wow sick summary2",
+    image: "hmmm ill work on this2",
+    isFavorite: false,
+};
+
+const tempBook3: Book = {
+    id: "192083745133",
+    title: "holy hell3",
+    author: "joe mama3",
+    excerpt: "this is a sick excerpt3",
+    summary: "wow sick summary3",
+    image: "hmmm ill work on this3",
+    isFavorite: false,
+};
+
+const tempBook4: Book = {
+    id: "192083745134",
+    title: "holy hell4",
+    author: "joe mama4",
+    excerpt: "this is a sick excerpt4",
+    summary: "wow sick summary4",
+    image: "hmmm ill work on this4",
+    isFavorite: false,
+};
 
 const CameraModal: React.FC<CameraModalProps> = ({ closeModal }) => {
     // const [cameraPermission, requestCameraPermission] = useCameraPermissions();
@@ -28,12 +80,25 @@ const CameraModal: React.FC<CameraModalProps> = ({ closeModal }) => {
             quality: 1,
         });
 
-        setIsProcessing(false);
         if (!result.canceled) {
-            await uploadImage(result.assets[0].uri);
+            // console.log("start wait"); // for testing activity indicator
+            // await sleep(5000); // for testing activity indicator
+            // console.log("waited 5 seconds"); // for testing activity indicator
+            // await uploadImage(result.assets[0].uri); 
             closeModal();
+            userPickBook();
         }
+        setIsProcessing(false);
     };
+
+    // for testing activity indicator
+    // async function sleep(ms: number): Promise<void> {
+    //     return new Promise((resolve) => setTimeout(resolve, ms));
+    // }
+
+    const userPickBook = () => {
+
+    }
 
     // Handle picking an image from the gallery by requesting permissions before picking the image if necessary
     const handlePickImage = async () => {
@@ -53,6 +118,7 @@ const CameraModal: React.FC<CameraModalProps> = ({ closeModal }) => {
         if (!result.canceled) {
             await uploadImage(result.assets[0].uri);
             closeModal();
+            userPickBook();
         }
         setIsProcessing(false);
     };
@@ -77,8 +143,25 @@ const CameraModal: React.FC<CameraModalProps> = ({ closeModal }) => {
 
             // TODO: if response is good show 4 options for user
             if (response.ok) {
+                console.log('success');
                 const result = await response.json();
-                console.log("upload successful", result);
+                // console.log("result: ", result.results[0])
+                if (result.results[0]) {
+                    const book1 = jsonToBook(result.results[0]);
+                    console.log("book1: ", book1);
+                }
+                if (result.results[1]) {
+                    const book1 = jsonToBook(result.results[1]);
+                    console.log("book2: ", book1);
+                }
+                if (result.results[2]) {
+                    const book1 = jsonToBook(result.results[2]);
+                    console.log("book3: ", book1);
+                }
+                if (result.results[3]) {
+                    const book1 = jsonToBook(result.results[3]);
+                    console.log("book4: ", book1);
+                }
             } else {
                 console.error("error uploading image: ", response.status);
                 const errorText = await response.text();
@@ -89,12 +172,28 @@ const CameraModal: React.FC<CameraModalProps> = ({ closeModal }) => {
         }
     };
 
+    // returns a Book object from json given by google books
+    const jsonToBook = (bookjson: apiReturn) => {
+        const returnBook: Book = {
+            id: bookjson.isbn,
+            title: bookjson.title,
+            author: bookjson.authors,
+            excerpt: bookjson.excerpt,
+            summary: bookjson.summary,
+            image: bookjson.thumbnail,
+            // rating: bookjson.rating, (cant do rating like this)
+            isFavorite: false,
+        }
+        return returnBook;
+    }
+
     return (
         <Modal animationType="fade" transparent visible>
             <TouchableWithoutFeedback onPress={closeModal}>
                 <View className="flex-1 justify-center items-center">
                     <TouchableWithoutFeedback>
                         <View className="bg-white rounded-lg w-64 p-4 space-y-4">
+                            {isProcessing && <ActivityIndicator size='large' color='#0000ff' />}
                             <Pressable
                                 onPress={handleTakePicture}
                                 disabled={isProcessing}
